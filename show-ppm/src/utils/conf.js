57ca3d90c02bed17b2953a43aaede7ce9503342c
luosanshao <lpj_1996@163.com>
2019-05-10 16:27:56 +0800

    '更改配置文件'

--- show-ppm/src/utils/conf.js
+++ show-ppm/src/utils/conf.js
@@ -8,21 +8,7 @@ export function showurl() { //前端地址
     return 'http://127.0.0.1:10888'
 }
 
-// //服务器上
-// export function showurl() { //前端地址
-//     return 'http://39.105.211.25'
-// }
-// export  function apiurl() {//后端api
-//     return 'http://39.105.211.25:10800'
-// }
 
-//本地穿透
-// export  function apiurl() { //后端api
-//     return 'http://ppmapi.mryl-frp.leconginfo.com'
-// }
-// export function showurl() { //前端地址
-//     return 'http://ppmshow.mryl-frp.leconginfo.com'
-// }
 
 
 
